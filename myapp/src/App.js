f1cfb5172e2a37903f5b12c44a7ae28e4afdbad5
MohamedTlili <Mohamedtlili1995@hotmail.com>
2023-11-10 03:25:23 +0100

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import { Card, Icon, Image } from "semantic-ui-react";
+function App() {
+  return (
+    <div className="App">
+      <Card>
+        <Image
+          src="https://static.vecteezy.com/system/resources/previews/020/765/399/non_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg"
+          wrapped
+          ui={false}
+        />
+        <Card.Content>
+          <Card.Header>Matthew</Card.Header>
+          <Card.Meta>
+            <span className="date">Joined in 2015</span>
+          </Card.Meta>
+          <Card.Description>
+            Matthew is a musician living in Nashville.
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a>
+            <Icon name="user" />
+            22 Friends
+          </a>
+        </Card.Content>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
